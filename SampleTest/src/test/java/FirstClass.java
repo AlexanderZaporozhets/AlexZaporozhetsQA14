e01bc35962006017de616807a73a3b4fc11c8486
AlexanderZaporozhets <38666390+AlexanderZaporozhets@users.noreply.github.com>
2018-04-27 12:03:54 +0300

    created first test

---
+++ SampleTest/src/test/java/FirstClass.java
@@ -0,0 +1,50 @@
+package com.example.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class FirstClass {
+  private WebDriver driver;
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    driver = new ChromeDriver();
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void searchWebDriverInChrome() throws Exception {
+    driver.get("https://www.google.com/");
+    driver.findElement(By.id("lst-ib")).click();
+    driver.findElement(By.id("lst-ib")).clear();
+    driver.findElement(By.id("lst-ib")).sendKeys("webDriver");
+    driver.findElement(By.id("lst-ib")).sendKeys(Keys.ENTER);
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+    }
